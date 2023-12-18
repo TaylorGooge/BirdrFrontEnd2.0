2b5f2ab8cc58bbc7f5377a13efd9e0b71019557c
TaylorGooge <taylorgooge@gmail.com>
2023-12-18 21:14:28 +0000

    improve gallery carousel

--- src/Components/Learn/BirdExplore/Gallery.jsx
+++ src/Components/Learn/BirdExplore/Gallery.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Carousel from 'react-bootstrap/Carousel';  
+import Image from 'react-bootstrap/Image'
 
 const Gallery = (props) => {
 
-
   return (
-  
-         <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+    <Carousel>
       {props.images.map((image, index) => (
-        <ImageListItem key={index}>
-          <img
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-         
-            loading="lazy"
-          />
-        </ImageListItem>
+      <Carousel.Item key={index}>
+        <Image src={image} style = {{maxWidth: '100%', height: 'auto'}} thumbnail/>
+      </Carousel.Item>
       ))}
-    </ImageList>
+    </Carousel>
+  
+    
 
 
 
